5fb1fe4b1cae7620dba49afd52c83b159ca7f1dc
AlejandroBiga <81267568+AlejandroBiga@users.noreply.github.com>
2026-02-19 18:30:13 -0300

    cf

---
+++ Assets/DOTS/LoadingManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadingManager : MonoBehaviour
+{
+    public string sceneToLoad = "loadingscene";
+
+    void Start()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneToLoad);
+        op.allowSceneActivation = false;
+
+        while (op.progress < 0.9f)
+            yield return null;
+
+        op.allowSceneActivation = true;
+    }
+}
